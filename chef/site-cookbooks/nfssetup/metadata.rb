d465f730addf9466df0fb126119f00fab173a339
Hiroyuki Sato <hiroyuki.sato@imsbio.co.jp>
2017-06-09 13:59:19 +0900

    modify nfs setup

---
+++ chef/site-cookbooks/nfssetup/metadata.rb
@@ -0,0 +1,7 @@
+name             'nfssetup'
+maintainer       'Hiroyuki SATO'
+maintainer_email 'Hiroyuki SATO'
+license          'GPL 2.0'
+description      'Installs/Configures nfssetup'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
